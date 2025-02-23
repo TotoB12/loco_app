b8d1c02fc844c305b5493d987ccacb242f23ea22
TotoB12 <totobeliard@outlook.com>
2025-02-22 23:05:30 -0500

    Remove unused iOS files and update Mapbox version in gradle.properties

--- app.config.js
+++ app.config.js
@@ -24,9 +24,8 @@ export default ({ config }) => {
         "react-native-radar",
         {
           "iosFraud": false,
-          "iosNSLocationWhenInUseUsageDescription": "We use your location to help share your position in real-time.",
-          "iosNSLocationAlwaysUsageDescription": "Background location is used so your friends can see your location even if the app is closed.",
-          "iosNSLocationAlwaysAndWhenInUseUsageDescription": "Background location is used so your friends can see your location even if the app is closed.",
+          // "iosNSLocationWhenInUseUsageDescription": "We use your location to help share your position in real-time.",
+          // "iosNSLocationAlwaysAndWhenInUseUsageDescription": "Background location is used so your friends can see your location even if the app is closed.",
           "iosBackgroundMode": true,
           "androidFraud": false,
           "androidBackgroundPermission": true,
@@ -38,15 +37,15 @@ export default ({ config }) => {
         "@rnmapbox/maps",
         {
           "RNMapboxMapsDownloadToken": process.env.MAPBOX_DOWNLOADS_TOKEN,
-          "RNMapboxMapsVersion": "11.0.0"
+          "RNMapboxMapsVersion": "11.8.0"
         }
       ],
       [
         "expo-location",
         {
-          "locationWhenInUsePermission": "Show current location on map.",
+          // "locationWhenInUsePermission": "Show current location on map.",
           // "locationAlwaysPermission": "Background location usage...",
-          "locationAlwaysAndWhenInUsePermission": "Allow Loco to use your location.",
+          // "locationAlwaysAndWhenInUsePermission": "Allow Loco to use your location.",
           "isIosBackgroundLocationEnabled": true,
           "isAndroidBackgroundLocationEnabled": true
         }
@@ -64,6 +63,7 @@ export default ({ config }) => {
     "ios": {
       "buildNumber": "0.1.5",
       "supportsTablet": true,
+      "bundleIdentifier": "com.antoninbeliard.loco",
       "infoPlist": {
         "UIBackgroundModes": [
           "location",
@@ -71,7 +71,7 @@ export default ({ config }) => {
         ],
         "NSLocationWhenInUseUsageDescription": "We need your location to show your position on the map and share it with friends.",
         "NSLocationAlwaysAndWhenInUseUsageDescription": "We need background location access to share your live location with friends, even when the app is in the background or closed.",
-        // "NSLocationAlwaysUsageDescription": "We need background location access to share your live location with friends, even when the app is in the background or closed." // Include this for older iOS versions, even though it's deprecated.
+        "NSLocationAlwaysUsageDescription": "We need background location access to share your live location with friends, even when the app is in the background or closed." // Include this for older iOS versions, even though it's deprecated.
       }
     },
     "android": {
@@ -84,6 +84,14 @@ export default ({ config }) => {
         "FOREGROUND_SERVICE"
       ]
     },
+    "permissions": [
+      "ACCESS_COARSE_LOCATION",
+      "ACCESS_FINE_LOCATION",
+      "ACCESS_BACKGROUND_LOCATION",
+      "FOREGROUND_SERVICE",
+      "FOREGROUND_SERVICE_LOCATION",
+      "WAKE_LOCK"
+    ],
     "extra": {
       "eas": {
         "projectId": "eacea42d-f4e8-4a94-b43a-f19dc934865e"
